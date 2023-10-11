7c7cbbf0e3ff36ce051d5e570ff7adc2dfc0fd60
kimkangyun <rlarkddbs3@osstem.com>
2023-10-11 13:27:33 +0900

    'init'

---
+++ src/test/java/com/example/noaxure/NoAxureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noaxure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoAxureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
